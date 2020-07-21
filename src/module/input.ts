314d13eb0c0d98c4859cf22767250ac4c4108d04
蒋毅 <jiangyi@bertadata.com>
2020-07-21 14:15:16 +0800

    Update

--- src/module/input.ts
+++ src/module/input.ts
@@ -23,6 +23,28 @@ const defaultOption: Option = {
  * @return 选项
  */
 function input(option: Option): Option {
+  let { name, input, output, format, prefix } = option
+
+  if (name && !(typeof name === 'string')) {
+    throw new Error('参数 name 错误')
+  }
+  if (input && !(typeof input === 'string')) {
+    throw new Error('参数 input 错误')
+  }
+  if (output && !(typeof output === 'string')) {
+    throw new Error('参数 output 错误')
+  }
+  if (format) {
+    if (!Array.isArray(format)) {
+      throw new Error('参数 format 错误')
+    } else if (format.length === 0) {
+      option.format = defaultOption.format
+    }
+  }
+  if (prefix && !(typeof prefix === 'string')) {
+    throw new Error('参数 prefix 错误')
+  }
+
   option = Object.assign({}, defaultOption, option)
 
   let cwd = process.cwd()
